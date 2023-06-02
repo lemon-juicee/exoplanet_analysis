d1cf0a360d15beef92cba4e01a75d74f657926db
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-06-01 17:09:55 -0700

    Develop model for averaging repeating values

--- exoplanet_analysis.py
+++ exoplanet_analysis.py
@@ -5,4 +5,13 @@ import seaborn as sns
 import scipy.stats as stats
 
 
-data_table = pandas.read_csv("radius_with_names.csv")
+data_table = np.array(pandas.read_csv("radius_with_names.csv"))
+data_dict = {}
+
+for planet in data_table[0:, 0]:
+    where = np.where(data_table[0:, 0] == planet)
+    cum = []
+    for place in where:
+        cum.append(data_table[place, 1])
+    data_dict[str(planet)] = np.mean(cum)
+
