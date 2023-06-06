9aa1ef4285323488b2d245fdf4f3caf967cc49ee
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-06-06 12:03:54 -0700

    Develop class-based functionality

--- exoplanet_analysis.py
+++ exoplanet_analysis.py
@@ -5,6 +5,17 @@ import seaborn as sns
 import scipy.stats as stats
 
 
+class Parameter:
+    def __init__(self, file, earth_value):
+        self.data = np.array(pandas.read_csv(file))
+        self.dict = {planet:parameter for [planet, parameter] in self.data}
+        self.ev = earth_value
+
+
+radius = Parameter('radius_with_names.csv', 1)
+
+
+'''
 data_table = np.array(pandas.read_csv("radius_with_names.csv"))
 data_dict = {planet:radius for [planet, radius] in data_table}
 earth_value = 1
@@ -12,3 +23,4 @@ earth_value = 1
 sns.kdeplot([planet[1] for planet in data_table])
 plt.axvline(earth_value, 0, 1, color = 'black')
 plt.show()
+'''
