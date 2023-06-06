7aca55640c38764c2958efe197bd27e2653ea2ff
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-06-06 12:32:15 -0700

    Remove commented code

--- exoplanet_analysis.py
+++ exoplanet_analysis.py
@@ -30,14 +30,3 @@ class Parameter:
         plt.show()
 
 radius = Parameter('radius_with_names.csv', 1)
-
-
-'''
-data_table = np.array(pandas.read_csv("radius_with_names.csv"))
-data_dict = {planet:radius for [planet, radius] in data_table}
-earth_value = 1
-
-sns.kdeplot([planet[1] for planet in data_table])
-plt.axvline(earth_value, 0, 1, color = 'black')
-plt.show()
-'''
