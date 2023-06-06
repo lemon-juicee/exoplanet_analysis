7496d1263c8d90781d525afabba04ef8d5828372
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-06-06 12:04:18 -0700

    Implement first test of plot()

--- exoplanet_analysis.py
+++ exoplanet_analysis.py
@@ -10,7 +10,24 @@ class Parameter:
         self.data = np.array(pandas.read_csv(file))
         self.dict = {planet:parameter for [planet, parameter] in self.data}
         self.ev = earth_value
-
+    def plot(self, color = 'blue', show_earth = False, earth_color = 'black'):
+        values = np.array(self.data[0:,1])
+        median = np.median(values)
+        sns.kdeplot(values, fill = True)
+        ax = sns.kdeplot(values)
+        kde_x = ax.get_lines()[-1].get_xdata()
+        kde_y = ax.get_lines()[-1].get_ydata()
+        if median > self.ev and show_earth:
+            mask = kde_x < self.ev
+            filled_x, filled_y = kde_x[mask], kde_y[mask]
+            ax.fill_between(filled_x, y1=filled_y)
+            earth_line = plt.axvline(self.ev, 0, 1, color = earth_color)
+        elif median < self.ev and show_earth:
+            mask = kde_x > self.ev
+            filled_x, filled_y = kde_x[mask], kde_y[mask]
+            ax.fill_between(filled_x, y1=filled_y)
+            earth_line = plt.axvline(self.ev, 0, 1, color = earth_color)
+        plt.show()
 
 radius = Parameter('radius_with_names.csv', 1)
 
