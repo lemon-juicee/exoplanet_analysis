704058069073fad3e67e04b522d405e988c66771
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-07-10 16:06:27 -0700

    Add check_for_nan()

---
+++ debug.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import exoplanet_analysis
+
+def check_for_nan(array):
+    """Check for NaN locations within an array."""
+    nan_locations = []
+    number = 0
+    for value in array:
+        if np.isnan(value):
+            nan_locations.append(number)
+            number += 1
+    return nan_locations
