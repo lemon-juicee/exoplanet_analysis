ac193f316655123949ce7fd2a6b452796081a45e
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-07-13 18:08:13 -0700

    Create apparent_brightness() formula

--- transit_formulas.py
+++ transit_formulas.py
@@ -4,4 +4,11 @@ import pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+def apparent_brightness():
+    data = np.array(pandas.read_csv('pl_name,pl_orbsmax,st_num.csv'))
+    brightness_dict = {}
+    for name, orb, lum in data:
+            lum = float(10) **  lum
+            brightness = (149597870691 * orb) / (4. * float(math.pi) * float(lum) * 3.826 * 10**26)
+            brightness_dict[name] = brightness
+    return brightness_dict
