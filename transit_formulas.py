7fe9951ea7a2c4c9f84e43fc2f6e88ce40350574
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-07-14 20:40:27 -0700

    Implement brightness_decrease()

--- transit_formulas.py
+++ transit_formulas.py
@@ -12,3 +12,11 @@ def apparent_brightness():
         brightness = (149597870691 * orb) / (4. * float(math.pi) * float(lum) * 3.826 * 10**26)
         brightness_dict[name] = brightness
     return brightness_dict
+
+def brightness_decrease():
+    data = np.array(pandas.read_csv('pl_name,pl_rade,st_rad.csv'))
+    brightness_loss = {}
+    for name, pl_rade, st_rad in data:
+        loss = ((pl_rade * 6378137) ** 2) / ((st_rad * 695700000) ** 2)
+        brightness_loss[name] = loss
+    return brightness_loss
