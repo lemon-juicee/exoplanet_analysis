c60d323e29cf91659fff76dbe566b290e5ffd94f
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-07-22 07:52:44 -0700

    Utilize tools, allow for variety when calling brightness_decrease

--- transit_formulas.py
+++ transit_formulas.py
@@ -3,9 +3,10 @@ import math
 import pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tools
 
 def apparent_brightness():
-    data = np.array(pandas.read_csv('pl_name,pl_orbsmax,st_num.csv'))
+    data = tools.data_from_csv('pl_name,pl_orbsmax,st_num.csv')
     brightness_dict = {}
     for name, orb, lum in data:
         lum = float(10) **  lum
@@ -13,10 +14,11 @@ def apparent_brightness():
         brightness_dict[name] = brightness
     return brightness_dict
 
-def brightness_decrease():
-    data = np.array(pandas.read_csv('pl_name,pl_rade,st_rad.csv'))
+def brightness_decrease(name):
+    data = tools.data_from_csv(name)
     brightness_loss = {}
     for name, pl_rade, st_rad in data:
         loss = ((pl_rade * 6378137) ** 2) / ((st_rad * 695700000) ** 2)
         brightness_loss[name] = loss
     return brightness_loss
+
